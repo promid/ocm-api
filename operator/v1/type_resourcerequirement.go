3e70388338ae832b7d232c1137fc1c3f7c153615
Dong Beiqing <350758787@qq.com>
2024-01-19 15:52:32 +0800

    move ResourceRequirement to another file

---
+++ operator/v1/type_resourcerequirement.go
@@ -0,0 +1,36 @@
+package v1
+
+import corev1 "k8s.io/api/core/v1"
+
+type ResourceRequirementRequire interface {
+	GetResourceRequirement() *ResourceRequirement
+}
+
+// ResourceRequirement allow user override the default pod QoS classes
+type ResourceRequirement struct {
+	// +kubebuilder:validation:Enum=Default;BestEffort;ResourceRequirement
+	// +kubebuilder:default:=Default
+	Type ResourceQosClass `json:"type"`
+	// ResourceRequirements defines resource requests and limits when Type is ResourceQosClassResourceRequirement
+	// +optional
+	ResourceRequirements *corev1.ResourceRequirements `json:"resourceRequirements,omitempty"`
+}
+
+type ResourceQosClass string
+
+const (
+	// Default use resource setting in the template file (with requests but no limits in the resources)
+	ResourceQosClassDefault ResourceQosClass = "Default"
+	// If all containers in the pod don't set resource request and limits, the pod is treated as BestEffort.
+	ResourceQosClassBestEffort ResourceQosClass = "BestEffort"
+	// Configurable resource requirements with requests and limits
+	ResourceQosClassResourceRequirement ResourceQosClass = "ResourceRequirement"
+)
+
+func (k *Klusterlet) GetResourceRequirement() *ResourceRequirement {
+	return k.Spec.ResourceRequirement
+}
+
+func (cm *ClusterManager) GetResourceRequirement() *ResourceRequirement {
+	return cm.Spec.ResourceRequirement
+}
